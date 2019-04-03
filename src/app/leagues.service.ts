177cba401d7a754f831d59cc03a1f80faf1dfe68
john-best1 <besty_2006@hotmail.co.uk>
2019-04-03 21:16:21 +0100

    updated league css, created multiple league and form type filters

--- src/app/leagues.service.ts
+++ src/app/leagues.service.ts
@@ -11,9 +11,9 @@ export class LeaguesService {
   league: Promise<Observable<any>>;
   constructor(private http: HttpClient) { }
 
-  get(): Observable<any>{
+  get(leagueCode: string): Observable<any>{
     const headers = new HttpHeaders().set('X-Auth-Token', '7830c352850f4acda78aa61d1666d45b');
-    return this.http.get("https://api.football-data.org/v2/competitions/PL/standings", {headers});
+    return this.http.get("https://api.football-data.org/v2/competitions/" + leagueCode + "/standings", {headers});
       
   }
 }
--- src/app/leagues/leagues.component.ts
+++ src/app/leagues/leagues.component.ts
@@ -3,6 +3,7 @@ import { LeaguesService } from './../leagues.service';
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { Observable } from 'rxjs';
 import { MatTableDataSource, MatSort } from '@angular/material';
+import { stringify } from '@angular/core/src/util';
 
 @Component({
   selector: 'leagues',
@@ -12,35 +13,60 @@ import { MatTableDataSource, MatSort } from '@angular/material';
 export class LeaguesComponent {
 
   league: Observable<any>;
+  leagueCode: string = "PL";
+  tableType: number = 0;
   leagueEntries: LeagueEntry[] = [];
   listData: MatTableDataSource<LeagueEntry>;
-  displayedColumns: string[] = ['position', 'name', 'matches', 'won', 'drawn', 'lost', 'gf', 'ga', 'gd', 'points'];
+  displayedColumns: string[] = ['position', 'name', 'matchesPlayed', 'won', 'drawn', 'lost', 'goalsFor', 'goalsAgainst', 'goalDifference', 'points'];
   entry: LeagueEntry;
+  leagues = [{id: 1, name: 'Premier League', code: 'PL'}, {id: 2, name: 'Ligue 1', code: 'FL1'},
+            {id: 3, name: 'Championship', code: 'ELC'}, {id: 4, name: 'Brazillian Division One', code: 'BSA'},
+            {id: 5, name: 'Bundesliga', code: 'BL1'},{id: 6, name: 'Italy Serie A', code: 'SA'},
+            {id: 7, name: 'Eredivisie', code: 'DED'},{id: 8, name: 'Portuguese Primera Division', code: 'PPL'},
+            {id: 9, name: 'Spain La Liga', code: 'PD'}];
+  formTypes = [{id: 0, name: 'Total'},{id: 1, name: 'Home'},{id: 2, name: 'Away'}];
 
   @ViewChild(MatSort) sort: MatSort;
 
   constructor(private leagueService: LeaguesService) { 
+      this.getTable()
+    }
 
-    this.league = leagueService.get();
-    this.league.subscribe(res => {
-      for(var i=0; i < res["standings"][0]["table"].length; i++){
-        this.entry = new LeagueEntry();
-        this.entry.position = res["standings"][0]["table"][i]["position"];
-        this.entry.name = res["standings"][0]["table"][i]["team"]["name"];
-        this.entry.matchesPlayed = res["standings"][0]["table"][i]["playedGames"];
-        this.entry.won = res["standings"][0]["table"][i]["won"];
-        this.entry.drawn = res["standings"][0]["table"][i]["draw"];
-        this.entry.lost = res["standings"][0]["table"][i]["lost"];
-        this.entry.goalsFor = res["standings"][0]["table"][i]["goalsFor"];
-        this.entry.goalsAgainst = res["standings"][0]["table"][i]["goalsAgainst"];
-        this.entry.goalDifference = res["standings"][0]["table"][i]["goalDifference"];
-        this.entry.points = res["standings"][0]["table"][i]["points"];     
-        this.leagueEntries.push(this.entry);
-        this.initializeTable(this.leagueEntries);
-      }
-    })
 
-    
+    getTable(){
+      this.league = this.leagueService.get(this.leagueCode);
+      this.populateLeagueEntries(this.tableType);
+    }
+
+    populateLeagueEntries(type: number){
+      this.league.subscribe(res => {
+        for(var i=0; i < res["standings"][type]["table"].length; i++){
+          this.entry = new LeagueEntry();
+          this.entry.position = res["standings"][type]["table"][i]["position"];
+          this.entry.name = res["standings"][type]["table"][i]["team"]["name"];
+          this.entry.matchesPlayed = res["standings"][type]["table"][i]["playedGames"];
+          this.entry.won = res["standings"][type]["table"][i]["won"];
+          this.entry.drawn = res["standings"][type]["table"][i]["draw"];
+          this.entry.lost = res["standings"][type]["table"][i]["lost"];
+          this.entry.goalsFor = res["standings"][type]["table"][i]["goalsFor"];
+          this.entry.goalsAgainst = res["standings"][type]["table"][i]["goalsAgainst"];
+          this.entry.goalDifference = res["standings"][type]["table"][i]["goalDifference"];
+          this.entry.points = res["standings"][type]["table"][i]["points"];     
+          this.leagueEntries.push(this.entry);
+          this.initializeTable(this.leagueEntries);
+        }})
+    }
+
+    onChangeLeague(leagueId){
+    this.leagueCode = this.leagues.filter(l => l.id == leagueId)[0].code;
+    this.leagueEntries = [];
+    this.getTable();
+  }
+
+  onChangeForm(formTypeId){
+    this.tableType = this.formTypes.filter(f => f.id == formTypeId)[0].id;
+    this.leagueEntries = [];
+    this.populateLeagueEntries(this.tableType);
   }
 
   private initializeTable(entries: LeagueEntry[]){
